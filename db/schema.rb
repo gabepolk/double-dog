19e0541201d91bfb6a3b1bca96b0b2a1fd56f06d
Gabe Polk <gabe.a.polk@gmail.com>
2014-07-21 08:54:56 -0700

    Added items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721084402) do
+ActiveRecord::Schema.define(version: 20140721085220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: true do |t|
+    t.string  "name"
+    t.integer "price"
+  end
 
   create_table "users", force: true do |t|
     t.string  "username"
